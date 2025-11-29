d426867ab731473498185d523268a3141996abd8
Marcell Nathan Santoso <marcellnathansantoso@gmail.com>
2025-11-29 22:22:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,32 @@ import numpy as np
 import tempfile
 import urllib.request
 from ultralytics import YOLO
+import os
 
 st.title("♻️ Waste Detection YOLO")
 
-MODEL_DRIVE_URL = "https://drive.google.com/file/d/1DSp9TnRA_twScvL-Ds84vrMO9iYOPfid/view?usp=sharing"
+# === LINK HUGGING FACE ===
+MODEL_HF_URL = "https://huggingface.co/Marcellfevaveavav/YoloV11m/resolve/main/waste_model.pt"
 MODEL_PATH = "waste_model.pt"
 
 @st.cache_resource
 def download_model():
-    try:
-        urllib.request.urlretrieve(MODEL_DRIVE_URL, MODEL_PATH)
-        return True
-    except Exception as e:
-        st.error(f"Gagal download model: {e}")
-        return False
+    if not os.path.exists(MODEL_PATH):
+        try:
+            urllib.request.urlretrieve(MODEL_HF_URL, MODEL_PATH)
+            st.success("✅ Model berhasil di-download dari Hugging Face")
+        except Exception as e:
+            st.error(f"Gagal download model: {e}")
+    return MODEL_PATH
 
 @st.cache_resource
 def load_model():
-    download_model()
-    return YOLO(MODEL_PATH)
+    path = download_model()
+    return YOLO(path)
 
 model = load_model()
 
 st.subheader("🟢 Pilih Mode")
-
 mode = st.radio("Mode Detection:", ["Realtime Webcam", "Upload Foto"])
 
 # ============================
@@ -37,14 +39,11 @@ if mode == "Realtime Webcam":
     st.write("Nyalakan kamera lalu arahkan sampah ke kamera.")
 
     run = st.checkbox("Start Webcam")
-
     FRAME_WINDOW = st.image([])
-
     cap = None
 
     if run:
         cap = cv2.VideoCapture(0)
-
         if not cap.isOpened():
             st.error("❌ Webcam gagal dibuka. Izinkan akses kamera.")
         else:
@@ -55,9 +54,7 @@ if mode == "Realtime Webcam":
                     break
 
                 results = model.predict(frame, imgsz=640, conf=0.5)
-
                 annotated = results[0].plot()
-
                 FRAME_WINDOW.image(annotated, channels="BGR")
 
     if cap:
@@ -68,16 +65,12 @@ if mode == "Realtime Webcam":
 # ============================
 elif mode == "Upload Foto":
     uploaded = st.file_uploader("Upload foto", type=["jpg", "jpeg", "png"])
-
     if uploaded:
         temp = tempfile.NamedTemporaryFile(delete=False)
         temp.write(uploaded.read())
         temp_path = temp.name
 
         img = cv2.imread(temp_path)
-
         results = model.predict(img, imgsz=640, conf=0.5)
-
         annotated = results[0].plot()
-
         st.image(annotated, channels="BGR", caption="Hasil Deteksi")
